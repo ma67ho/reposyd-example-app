68656ed05804d6e8356217011ed32b49832f988c
Martin Hoppe <48030323+ma67ho@users.noreply.github.com>
2026-03-04 14:58:11 +0100

    Added table example

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,23 @@
 <template>
   <q-page class="row items-center justify-evenly">
+    <q-table
+    :columns="tableColumns"
+    :rows="tableRows"
+    >
+    <template v-slot:body-cell="props">
+      <q-td key="calories" :props="props">
+            {{ props.value }}
+            <q-popup-edit v-model.number="props.row[props.col.field]" :title="props.col.label" buttons v-slot="scope">
+              <q-input type="number" v-model="scope.value" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+    </template>
+    <template v-slot:bottom-row="props">
+      <q-tr >
+      <q-th v-for="(col,index) of props.cols" :key="col.name">{{ footerValue[index as number] }}</q-th>
+      </q-tr>
+    </template>
+    </q-table>
     <example-component
       title="Example component"
       active
@@ -10,10 +28,44 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import type { Todo, Meta } from 'components/models';
 import ExampleComponent from 'components/ExampleComponent.vue';
+import { QTable, type QTableColumn } from 'quasar';
 
+const tableColumns = ref<QTableColumn[]>([
+  {
+    label: 'Stunden I',
+    name: 'col1',
+    field: 'stunden1'
+  },
+  {
+    label: 'Stunden II',
+    name: 'col2',
+    field: 'stunden2'
+  }
+])
+const tableRows = ref<{ stunden1: number, stunden2: number }[]>([
+  { stunden1: 1, stunden2: 0},
+  {stunden1: 0, stunden2: 1}
+])
+
+interface IStunden {
+  [key: string]: number;
+
+}
+const footerValue = computed(():number[] => {
+  const values:number[] = [] 
+  for(const col of tableColumns.value){
+    console.log(col)
+    values.push(tableRows.value.reduce((p: number, row:{ stunden1: number, stunden2: number } ) => {
+      const key = col.field as string
+      const v = (row as IStunden)[key] || 0
+      return p + v
+    }, 0))
+  }
+  return values
+})
 const todos = ref<Todo[]>([
   {
     id: 1,
